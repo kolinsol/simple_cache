70d643824efa6e54a82def45a61debbd519663dd
Yauheni Tsiarokhin <hihihiko00@gmail.com>
2018-08-13 11:24:56 +0300

    add distribution to mnesia

--- simple_cache/src/sc_store.erl
+++ simple_cache/src/sc_store.erl
@@ -6,13 +6,16 @@
          lookup/1]).
 
 -define(TABLE_ID, ?MODULE).
+-define(WAIT_FOR_TABLES, 2000).
 
 -record(key_to_pid, {key, pid}).
 
 init() ->
-    mnesia:create_table(key_to_pid,
-                        [{index, [pid]},
-                         {attributes, record_info(fields, key_to_pid)}]).
+    mnesia:stop(),
+    mnesia:delete_schema([node()]),
+    mnesia:start(),
+    {ok, CacheNodes} = rd_server:fetch_resource_locations(simple_cache),
+    dynamic_db_init(lists:delete(node(), CacheNodes)).
 
 insert(Key, Pid) ->
     mnesia:dirty_write(#key_to_pid{key = Key, pid = Pid}).
@@ -39,3 +42,21 @@ is_pid_alive(Pid) when node(Pid) =:= node() ->
 is_pid_alive(Pid) ->
     lists:member(node(Pid), nodes()) andalso
     (rpc:call(node(Pid), erlang, is_process_alive, [Pid]) =:= true).
+
+dynamic_db_init([]) ->
+    mnesia:create_table(key_to_pid,
+                        [{index, [pid]},
+                         {attributes, record_info(fields, key_to_pid)}]);
+dynamic_db_init(CacheNodes) ->
+    add_extra_nodes(CacheNodes).
+
+add_extra_nodes([Node|T]) ->
+    case mnesia:change_config(extra_db_nodes, [Node]) of
+        {ok, [Node]} ->
+            mnesia:add_table_copy(schema, node(), ram_copies),
+            mnesia:add_table_copy(key_to_pid, node(), ram_copies),
+            Tables = mnesia:system_info(tables),
+            mnesia:wait_for_tables(Tables, ?WAIT_FOR_TABLES);
+        _ ->
+            add_extra_nodes(T)
+    end.
